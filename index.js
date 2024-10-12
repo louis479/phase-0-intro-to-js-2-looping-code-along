e366a067e9eb82ac90944e0c015c51cd8316655f
louis-ngugi <louisngugi858@gmail.com>
2024-10-12 09:22:04 +0300

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+// Function to create thank you messages
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  
+  // Function to count down from a given number
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
